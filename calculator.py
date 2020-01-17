e6f239d9b33ed25a551d420a222b662d2fa87972
yl642 <yl642@duke.edu>
2020-01-17 17:09:43 -0500

    Finish calculator.py

--- calculator.py
+++ calculator.py
@@ -1,24 +1,37 @@
 # calculator.py
 
 def add(x, y):
-    z = x + y;
-    print("{} + {} = {}".format(x, y, z))
+    z = x + y
     return z
 
-x = input("Input a letter: ")
-print("You entered {}.".format(x))
-print("Here is a line of code.")
+def subtract(x, y):
+    z = x - y
+    return z
 
-if x == "a":
-    z = add(100, 2)
-    if z > 100:
-        print("This number is too high.")
+def multiply(x, y):
+    z = x * y;
+    return z
 
-elif x == "s":
-    a = 1;
-    b = 2;
-    c = a - b;
-    print("{} - {} = {}".format(a, b, c))
+def divide(x, y):
+    z = x / y;
+    return z
+
+x = input("Enter an operation type you want to make (+-*/): ")
+a = float(input("Enter First Number: "))
+b = float(input("Enter Second Number: "))
+
+if x == "+":
+    z = add(a, b)
+    print("{} {} {} = {}".format(a, x, b, z))
+elif x == "-":
+    z = subtract(a, b)
+    print("{} {} {} = {}".format(a, x, b, z))
+elif x == "*":
+    z = multiply(a, b)
+    print("{} {} {} = {}".format(a, x, b, z))
+elif x == "/":
+    z = divide(a, b)
+    print("{} {} {} = {}".format(a, x, b, z))
 else:
     print("The \"{}\" command is not recognized.".format(x))
 print("Done.")
